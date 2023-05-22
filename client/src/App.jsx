55419c2d2b4ecbab1e33143d87648a326c169e3f
uamadasun <uchennaamadasun@gmail.com>
2023-05-22 16:18:16 -0400

    Siteheader created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import SiteHeader from "./components/SiteHeader";
 
 
 function App() {
   return (
     <div className="App">
+      <SiteHeader/>
       {/* <h1>hello</h1> */}
       <Routes>
             <Route exact path="/" element={<Homepage />} />
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,110 +1,12 @@
 import React from 'react'
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import Logo from '../assets/logo.png'
-import LogoWhite from '../assets/logo-white.png'
-import Lightning from '../assets/lightning.png'
+
 
 const Homepage = () => {
 
-    
-    // const navigation = [
-    //   { name: 'Product', href: '#' },
-    //   { name: 'Features', href: '#' },
-    //   { name: 'Marketplace', href: '#' },
-    //   { name: 'Company', href: '#' },
-    // ]
-    
 
-      const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-    
       return (
-        <div className="bg-black">
-          <header className="absolute inset-x-0 top-0 z-50">
-            <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-              <div className="flex lg:flex-1">
-                <a href="#" className="-m-1.5 p-1.5">
-                  <span className="sr-only">Strikify</span>
-                  <img
-                    className="h-10 w-10"
-                    src={Lightning}
-                    alt=""
-                  />
-                </a>
-              </div>
-              <div className="flex lg:hidden">
-                <button
-                  type="button"
-                  className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
-                  onClick={() => setMobileMenuOpen(true)}
-                >
-                  <span className="sr-only">Open main menu</span>
-                  <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-                </button>
-              </div>
-              {/* <div className="hidden lg:flex lg:gap-x-12">
-                {navigation.map((item) => (
-                  <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-white">
-                    {item.name}
-                  </a>
-                ))}
-              </div> */}
-              
-            </nav>
-            <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-              <div className="fixed inset-0 z-50" />
-              <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white">
-                <div className="flex items-center justify-between">
-                  <a href="#" className="-m-1.5 p-1.5">
-                    <span className="sr-only">Strikify</span>
-                    <img
-                      className="h-20 w-20"
-                      src={LogoWhite}
-                      alt=""
-                    />
-                  </a>
-                  <button
-                    type="button"
-                    className="-m-2.5 rounded-md p-2.5 text-white"
-                    onClick={() => setMobileMenuOpen(false)}
-                  >
-                    <span className="sr-only">Close menu</span>
-                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-                </div>
-                <div className="mt-6 flow-root">
-                  <div className="-my-6 divide-y divide-white-500/10">
-                    {/* <div className="space-y-2 py-6">
-                      {navigation.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50 hover:text-black"
-                        >
-                          {item.name}
-                        </a>
-                      ))}
-                    </div> */}
-                    <div className="py-6">
-                      <a
-                        href="#"
-                        className="-mx-3 block rounded-lg px-7 py-2.5 text-base font-semibold leading-7 bg-white text-black hover:text-black hover:bg-yellow-300"
-                      >
-                        Log in
-                      </a>
-                      <a
-                        href="#"
-                        className="-mx-3 my-5 block rounded-lg px-7 py-2.5 text-base font-semibold leading-7 bg-white text-black hover:text-black hover:bg-yellow-300"
-                      >
-                        Sign Up Free
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </Dialog.Panel>
-            </Dialog>
-          </header>
+        <div>
+          
     
           <div className="relative isolate px-6 pt-14 lg:px-8">
             <div
