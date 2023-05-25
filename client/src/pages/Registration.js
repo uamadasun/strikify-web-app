bbe7b354349012bd6d34d55d1fc12941d45d36e9
uamadasun <uchennaamadasun@gmail.com>
2023-05-25 12:47:56 -0400

    update schema for email

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -91,6 +91,11 @@ const Registration = () => {
           Register for an account
         </h2>
       </div>
+      <div className="text-center">
+      <p>A Strike username is required to register.</p>
+      <p>Don't have a Strike account? Register for one <a href="https://strike.me/download/" target="_blank"> <span className="text-yellow-300 font-semibold">HERE</span>.</a></p>
+      </div>
+      
 
       <div className="mt-1 sm:mx-auto sm:w-full sm:max-w-sm">
         <form className="space-y-2" onSubmit={onSubmitHandler}>
@@ -110,7 +115,7 @@ const Registration = () => {
                 id="username"
                 name="username"
                 type="username"
-                // autoComplete="username"
+                autoComplete="username"
                 required
                 className="block w-full rounded-md border-0 pl-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-300 sm:text-sm sm:leading-6"
                 onChange={handleInputChange}
